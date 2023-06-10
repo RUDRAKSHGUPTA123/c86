60fc1a171ef5ed754ba6abab8a3f528ccf32d6fb
RUDRAKSHGUPTA123 <95760734+RUDRAKSHGUPTA123@users.noreply.github.com>
2023-06-10 11:51:40 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyB1plzuYMy0pCKyhZU2orq-uAojGK_SLlo",
+    authDomain: "story-telling-app-a80a8.firebaseapp.com",
+    databaseURL: "https://story-telling-app-a80a8-default-rtdb.firebaseio.com",
+    projectId: "story-telling-app-a80a8",
+    storageBucket: "story-telling-app-a80a8.appspot.com",
+    messagingSenderId: "405434397700",
+    appId: "1:405434397700:web:d1c31cb5eb9d45b6a2b151"
+  };
